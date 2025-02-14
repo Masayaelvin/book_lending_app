4598485085991db8265958dc61547665955b7979
Masaya Elvin <masayaelvin@gmail.com>
2025-02-14 15:35:18 +0300

    succesfull running test for borrowing and books errors fixed

--- test/models/borrowing_test.rb
+++ test/models/borrowing_test.rb
@@ -3,9 +3,10 @@ require "test_helper"
 class BorrowingTest < ActiveSupport::TestCase
   def setup
     @user = User.create(email: "user@example.com", password: "password")
-    @book = Book.create(title: "Sample Book", author: "John Doe", isbn: "1112223334", available: true)
+    @book = Book.create(title: "Sample Book", author: "John Doe", isbn: SecureRandom.uuid, available: true)
     @borrowing = Borrowing.new(user: @user, book: @book)
   end
+
 
   test "borrowing should be valid" do
     assert @borrowing.valid?
